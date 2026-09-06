73fefd6f7ecc2b8d1116e7d9b77c58331a9f6b30
Daniel Araujo Chaves <danielaraujo.da789@gmail.com>
2026-09-06 19:23:22 -0300

    ✨ New Feature: Adicionando o ADMIN

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import ProdutoDetalhe from './pages/produtos/ProdutoDetalhe'
 import Produtos from './pages/produtos/Produtos'
 import ProdutosSaudaveis from './pages/produtos/ProdutosSaudaveis'
 import ProtectedRoute from './routes/ProtectedRoute'
+import AdminRoute from './routes/AdminRoute'
 
 const INTRO_KEY =
   '@BOMbocado:intro'
@@ -50,16 +51,41 @@ function App() {
           <Navigate to="/login" replace />} />
       <Route path="/login" element={<Login />} />
       <Route path="/cadastro" element={<Cadastro />} />
-      <Route element={<ProtectedRoute />} >
-        <Route element={<Layout />} >
-          <Route path="/produtos" element={<Produtos />} />
-          <Route path="/produtos/saudaveis" element={<ProdutosSaudaveis />} />
-          <Route path="/produtos/cadastrar" element={<FormProduto />} />
-          <Route path="/produtos/editar/:id" element={<FormProduto />} />
-          <Route path="/produtos/deletar/:id" element={<DeletarProduto />} />
-          <Route path="/produtos/:id" element={<ProdutoDetalhe />} />
+      <Route element={<ProtectedRoute />}>
+      <Route element={<Layout />}>
+        <Route
+          path="/produtos"
+          element={<Produtos />}
+        />
+
+        <Route
+          path="/produtos/saudaveis"
+          element={<ProdutosSaudaveis />}
+        />
+
+        <Route
+          path="/produtos/:id"
+          element={<ProdutoDetalhe />}
+        />
+        
+        <Route element={<AdminRoute />}>
+          <Route
+            path="/produtos/cadastrar"
+            element={<FormProduto />}
+          />
+
+          <Route
+            path="/produtos/editar/:id"
+            element={<FormProduto />}
+          />
+
+          <Route
+            path="/produtos/deletar/:id"
+            element={<DeletarProduto />}
+          />
         </Route>
       </Route>
+    </Route>
       <Route path="*" element={
           <Navigate to="/login" replace />
         }
